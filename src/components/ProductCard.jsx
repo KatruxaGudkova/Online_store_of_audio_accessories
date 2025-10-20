8d2af6a003d426e0c6eacf4df72e5010121c19fc
katruxagudkova <135836664+KatruxaGudkova@users.noreply.github.com>
2025-10-20 12:46:42 +0000

    Сделала нижнюю часть карточки

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -3,15 +3,27 @@ import "./ProductCard.scss";
 function ProductCard({ product, onAdd }) {
   return (
     <div className="product-card">
-      <img src={product.image} alt={product.name} />
-      <h3>{product.name}</h3>
-      <div>
-        <span className="price">{product.price} ₽</span>
-        {product.oldPrice && (
-          <span className="old-price">{product.oldPrice} ₽</span>
-        )}
+      <img src={product.image} className="image" alt={product.name} />
+
+      <div className="product-info">
+        <div className="left">
+          <h3>{product.name}</h3>
+          <div className="rating">
+            <img src="/images/star.svg" className="star-icon" />
+            {product.rating}
+          </div>
+        </div>
+
+        <div className="right">
+          <div className="price-wrapper">
+            <span className="price">{product.price} ₽</span>
+            {product.oldPrice && (
+              <span className="old-price">{product.oldPrice} ₽</span>
+            )}
+          </div>
+        </div>
+        <button onClick={onAdd}>Купить</button>
       </div>
-      <button onClick={onAdd}>Купить</button>
     </div>
   );
 }
