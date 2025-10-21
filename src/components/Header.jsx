5203aeddeff91165752138023bfa0435dbf6ef3a
katruxagudkova <135836664+KatruxaGudkova@users.noreply.github.com>
2025-10-21 07:52:20 +0000

    Добавила иконки избранного и корзины с счётчиком.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,28 @@
 import "./Header.scss";
 
 function Header() {
-  return <header>QPICK</header>;
+    // временно захардкодила количество
+    const favoritesCount = 3;
+    const basketCount = 2;
+  return (
+    <header>
+      <div className="logo">QPICK</div>
+      <div className="navbar">
+        <div className="navbar_favorite">
+          <img src="/images/favorite.svg" className="favorite-icon" />
+          {favoritesCount > 0 && (
+            <span className="badge">{favoritesCount}</span>
+          )}
+        </div>
+        <div className="navbar_basket">
+          <img src="/images/basket.svg" className="basket-icon" />
+          {basketCount > 0 && (
+            <span className="badge">{basketCount}</span>
+          )}
+        </div>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
